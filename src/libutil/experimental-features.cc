ba9fdeddaa327ccf12c0c018100c60185c4b7b9d
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2022-04-11 10:20:36 +0200

    gc: Use the trace helper

--- src/libutil/experimental-features.cc
+++ src/libutil/experimental-features.cc
@@ -12,7 +12,7 @@ struct ExperimentalFeatureDetails
     std::string_view description;
 };
 
-constexpr std::array<ExperimentalFeatureDetails, 14> xpFeatureDetails = {{
+constexpr std::array<ExperimentalFeatureDetails, 15> xpFeatureDetails = {{
     {
         .tag = Xp::CaDerivations,
         .name = "ca-derivations",
@@ -220,6 +220,20 @@ constexpr std::array<ExperimentalFeatureDetails, 14> xpFeatureDetails = {{
         .description = R"(
             Allow parsing of timestamps in builtins.fromTOML.
         )",
+    },
+    {
+        .tag = Xp::ExternalGCDaemon,
+        .name = "external-gc-daemon",
+        .description = R"(
+            Make the garbage collector use an external daemon for the tracing.
+
+            This makes it possible to run a multi-user Nix daemon as a non-root
+            user (only the tracing daemon needs to be root), reducing the attack
+            surface a lot.
+
+            This requires more infrastructure and isn't directly supported by the
+            installer.
+        )"
     },
 }};
 
